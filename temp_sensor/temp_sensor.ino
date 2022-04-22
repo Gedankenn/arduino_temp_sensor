661ffd711544d0830a1ddf789fe4bb9c73726680
Barbudo <Gedankenn@gmail.com>
2022-04-22 18:26:21 -0300

    Nao consegui testar, esp ta podre

---
+++ temp_sensor/temp_sensor.ino
@@ -0,0 +1,71 @@
+// Import required libraries
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <Hash.h>
+//#include <ESPAsyncTCP.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+
+// Replace with your network credentials
+const char* ssid = "NOT_YOUR_WIFI";
+const char* password = "karalhodesenha";
+
+#define DHTPIN 5     // Digital pin connected to the DHT sensor
+
+// Uncomment the type of sensor in use:
+#define DHTTYPE    DHT11     // DHT 11
+//#define DHTTYPE    DHT22     // DHT 22 (AM2302)
+//#define DHTTYPE    DHT21     // DHT 21 (AM2301)
+
+DHT dht(DHTPIN, DHTTYPE);
+
+// current temperature & humidity, updated in loop()
+float t = 0.0;
+float h = 0.0;
+
+unsigned long previousMillis = 0;    // will store last time DHT was updated
+const long interval = 10000;  
+
+void setup(){
+  // Serial port for debugging purposes
+  Serial.begin(115200);
+  dht.begin();
+  
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println(".");
+  }
+}
+
+  void loop(){  
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you updated the DHT values
+    previousMillis = currentMillis;
+    // Read temperature as Celsius (the default)
+    float newT = dht.readTemperature();
+    // Read temperature as Fahrenheit (isFahrenheit = true)
+    //float newT = dht.readTemperature(true);
+    // if temperature read failed, don't change t value
+    if (isnan(newT)) {
+      Serial.println("Failed to read from DHT sensor!");
+    }
+    else {
+      t = newT;
+      Serial.println(t);
+    }
+    // Read Humidity
+    float newH = dht.readHumidity();
+    // if humidity read failed, don't change h value 
+    if (isnan(newH)) {
+      Serial.println("Failed to read from DHT sensor!");
+    }
+    else {
+      h = newH;
+      Serial.println(h);
+    }
+  }
+}
